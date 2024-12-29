5b897a016366d95379fcc36ab288de1148e990f3
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:47:30 +0800

    Update PRAK501-2410817310014-INDRA_SURYADILAGA.py

--- Soal-1/PRAK501-2410817310014-INDRA_SURYADILAGA.py
+++ Soal-1/PRAK501-2410817310014-INDRA_SURYADILAGA.py
@@ -1,6 +1,13 @@
 def MaxBilangan(a, b, c, d):
-  return max(a, b, c, d)
+  hasil = a
+  if b > hasil:
+    hasil = b
+  if c > hasil:
+    hasil = c
+  if d > hasil:
+    hasil = d
+  return hasil
 
 a, b, c, d = map(int, input().split())
-hasil = MaxBilangan(a, b, c, d)
+hasil = int(MaxBilangan(a, b, c, d))
 print(hasil)
